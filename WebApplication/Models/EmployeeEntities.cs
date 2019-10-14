d939b62fe4843b9aaafc0567906286bfa5da691a
Venkatesh <vkalva45@gmail.com>
2019-10-13 19:39:25 -0500

    adding the required validators to the fields of EmployeeEntities class

---
+++ WebApplication/Models/EmployeeEntities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreCURDMVC_Demo.Models
+{
+    public class EmployeeEntities
+    {
+        [Required]
+        public string Emp_Name { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string Department { get; set; }
+    }
+}
