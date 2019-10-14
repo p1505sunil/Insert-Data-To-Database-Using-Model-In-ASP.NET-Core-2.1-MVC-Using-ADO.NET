760868a1f4ff8fe1978d314e97eb804432c0f94f
Venkatesh <vkalva45@gmail.com>
2019-10-13 19:47:44 -0500

    handle database operations,business logic of Create operation.

---
+++ WebApplication/Controllers/Employee12Controller.cs
@@ -0,0 +1,12 @@
+﻿namespace AspNetCoreCURDMVC_Demo.Controllers
+{
+    public class Employee12Controller : Controller
+    {
+        EmployeeDBAccessLayer empdb = new EmployeeDBAccessLayer();
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }  
