89f40fd4cf82b50de592b163d140e41a6cfd8e21
Venkatesh <vkalva45@gmail.com>
2019-10-13 19:48:35 -0500

    updated code

--- WebApplication/Controllers/Employee12Controller.cs
+++ WebApplication/Controllers/Employee12Controller.cs
@@ -1,4 +1,11 @@
-﻿namespace AspNetCoreCURDMVC_Demo.Controllers
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreCURDMVC_Demo.Models;
+
+namespace AspNetCoreCURDMVC_Demo.Controllers
 {
     public class Employee12Controller : Controller
     {
@@ -9,4 +16,22 @@
         {
             return View();
         }
-    }  
+        [HttpPost]
+        public IActionResult Create([Bind] EmployeeEntities employeeEntities)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    string resp = empdb.AddEmployeeRecord(employeeEntities);
+                    TempData["msg"] = resp;
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["msg"] = ex.Message;
+            }
+            return View();
+        }
+    }
+}
